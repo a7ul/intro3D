d15444c0c87870894ea99bb677ee82454191a2f6
Atul <atul_r@external.mckinsey.com>
2015-08-16 03:48:48 +0530

    Git ignore

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,4 +2,4 @@
 /**
  * Created by atulr on 08/08/15.
  */
-intro3d.init3DCharacter('#intro-example','assets/models/boxguy.json','walkCycle','assets/models/boxguy.animations.json');
+intro3d.init3DCharacter('.intro-example','assets/models/boxguy.json','walkCycle','assets/models/boxguy.animations.json');
