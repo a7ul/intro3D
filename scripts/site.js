576ff083bd1e1d18f6b5821060d23911011b0187
Atul <atul_r@external.mckinsey.com>
2015-08-17 23:58:10 +0530

    Minified and finished dist creator

---
+++ scripts/site.js
@@ -0,0 +1,13 @@
+'use strict';
+/**
+ * Created by atulr on 08/08/15.
+ */
+
+introJs.intro3d.addCharacter({
+  name: 'boxguy',
+  charJsonPath: 'assets/models/boxguy.json',
+  charAnimeJsonPath: 'assets/models/boxguy.animations.json'
+});
+
+window.introInstance = introJs().setOption('tooltipClass', 'customDefault').start();
+introJs.intro3d.initialize(introInstance);
